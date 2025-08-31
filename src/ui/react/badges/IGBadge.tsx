43eb6c889c72414a456da599b27361d0716eb7d5
freddysae0 <freddysae00@gmail.com>
2025-08-31 19:06:54 +0200

    fix/Adding llink to the instagram link

--- src/ui/react/badges/IGBadge.tsx
+++ src/ui/react/badges/IGBadge.tsx
@@ -4,8 +4,10 @@ import Button from "../components/Button";
 
 export default function IGBadge() {
   return (
-    <Button leftIcon={<FaInstagram size={28} />} variant="outline" size="sm" className="fixed  top-3 right-3 z-20">
+    <a href="https://instagram.com/redblock.online" target="_blank" rel="noreferrer" >
+    <Button  leftIcon={<FaInstagram size={28} />} variant="outline" size="sm" className="fixed  top-3 right-3 z-20">
       <span>@redblock.online</span>
     </Button>
+    </a>
   );
 }
