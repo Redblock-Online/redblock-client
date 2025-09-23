518845fbecc63b8e8bd30ae1d70afc9188c1c6c2
freddysae0 <freddysae00@gmail.com>
2025-09-23 23:40:25 +0200

    Commenting the Login button for now

--- src/ui/react/navbar/index.tsx
+++ src/ui/react/navbar/index.tsx
@@ -1,9 +1,9 @@
-import LoginGoogleButton from "./LoginGoogleButton";
-
+/* import LoginGoogleButton from "./LoginGoogleButton";
+ */
 export default function Navbar() {
   return (
     <div className="fixed top-1 left-4 h-16  flex items-center z-10">
-      <LoginGoogleButton />
-    </div>
+{/*       <LoginGoogleButton />
+ */}    </div>
   )
 }
